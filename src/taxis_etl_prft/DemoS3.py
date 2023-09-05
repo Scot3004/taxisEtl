a96dca79b3432b1fc9b7c31a9ad30fdde8d091bb
Sergio Orozco <scot3004@gmail.com>
2023-09-05 18:35:47 -0500

    Reemplazado spark-submit por llamado a python

--- src/taxis_etl_prft/DemoS3.py
+++ src/taxis_etl_prft/DemoS3.py
@@ -7,11 +7,11 @@ from pyspark import SparkConf
 load_dotenv()
 
 conf = SparkConf()
-conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.3.5,com.amazonaws:aws-java-sdk-bundle:1.12.316')
-
+conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262')
 spark = SparkSession.builder.appName("DemoS3").config(conf=conf).getOrCreate()
 
 bucket = os.getenv("AWS_S3_BUCKET")
+print("Bucket {}".format(bucket))
 filenames = os.getenv("TAXIS_ETL_FILES").split(",")
 for key in filenames:
     print("s3a://{}/{}".format(bucket, key))
